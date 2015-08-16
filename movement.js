89493214fab9a48c9e04cfb755f19cb346ccdabb
Etienne Ott <eti.ott@gmail.com>
2015-08-16 10:45:38 +0200

    Simplified controls down to single switch of gravitational constant on any key event.

--- movement.js
+++ movement.js
@@ -1,18 +1,14 @@
 var posX = 500;
 var posY = 400;
+var grav = 5;
 
 function doMovement() {
     var dx = 0;
     var dy = 0;
 
-    dx -= keyStatus[1] ? 10 : 0;
-    dx += keyStatus[3] ? 10 : 0;
-    dy -= keyStatus[0] ? 10 : 0;
-    dy += keyStatus[2] ? 10 : 0;
-
-    if (dx != 0 && dy != 0) {
-        dy *= 0.7071067811;
-        dx *= 0.7071067811;
+    if (keyStatus == 1) {
+        keyStatus = 2;
+        grav *= -1;
     }
 
     posX += dx;
